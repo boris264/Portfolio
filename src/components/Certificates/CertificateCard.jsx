2e4e2cf4a6cb5929a5620591a6cda28f851b8b0e
boris264 <boristodorov12@gmail.com>
2023-10-25 11:26:50 +0300

    Added an <a> tag to the certificates, so they can be opened in a new tab. Certificate images now scale up on hovering.

--- src/components/Certificates/CertificateCard.jsx
+++ src/components/Certificates/CertificateCard.jsx
@@ -3,7 +3,9 @@ import styles from './CertificateCard.module.css';
 function CertificateCard({ certificateImageUrl, certificateTitle }) {
     return (
         <div className={styles.certificateCard}>
-            <img src={certificateImageUrl}></img>
+            <a target="_blank" href={certificateImageUrl}>
+                <img className={styles.certificateImage} src={certificateImageUrl}></img>
+            </a>
             <span>{certificateTitle}</span>
         </div>
     );
